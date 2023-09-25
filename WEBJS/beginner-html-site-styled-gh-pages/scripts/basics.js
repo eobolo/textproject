81ae5a58496cc1ce266ed5a5b61871b65e918519
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-09-25 13:45:37 +0300

    new updates

---
+++ WEBJS/beginner-html-site-styled-gh-pages/scripts/basics.js
@@ -0,0 +1,10 @@
+// Here we are declaring the variable
+let myVariable;
+// Now we are assigning the variable
+myVariable = "Emmanuel";
+/*
+  We can also declare and assign the variable like 
+  c and java programming language.
+*/
+let myString = "Obolo";
+myVariable;
